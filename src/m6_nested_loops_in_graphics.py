948b9c12d3084bd0f4e6d9c63de2329a57f1b7d9
hughesm1 <hughesm1@rose-hulman.edu>
2019-01-16 23:41:21 -0500

    DONE

--- src/m6_nested_loops_in_graphics.py
+++ src/m6_nested_loops_in_graphics.py
@@ -3,8 +3,8 @@ This project demonstrates NESTED LOOPS (i.e., loops within loops)
 in the context of TWO-DIMENSIONAL GRAPHICS.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Marcus Hughes-Oliver.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -83,6 +83,13 @@ def draw_L(window, circle, r, c):
     # TODO: 2. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
+    for i in range(r):
+        for j in range(3):
+            circle.attach_to(window)
+            circle.center.y = circle.center.y + circle.center.y
+    window.render()
+
+
 
 
 def run_test_draw_wall_on_right():
